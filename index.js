9d001d84b75a1f702a634c93ca6e82c5a3059a39
felixs-alt <114471609+felixs-alt@users.noreply.github.com>
2024-06-02 10:27:29 +0200

    a

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ const upload = require('./upload');
 app.post('/upload', upload.single('file'), async (req, res) => {
     res.end("uploaded")
 })
-app.post('/reset', async (req, res) => {
-    res.end("restarted")
+app.post('/reset', (req, res) => {
     execRoot('./PiFmRds/src/pi_fm_rds',['-freq','97.0', '-audio', 'upload/music.wav'])
+    res.end("restarted")
 })
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,"index.html"));
